d61a23025662da6f4a25c9a8ad39fb44c08bf691
Bartosz Skorupa <asar.bs@gmail.com>
2016-07-10 22:01:28 +0200

    add tests for simple versions of timer.

---
+++ project/TimerV2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by asar on 10.07.16.
+//
+
+#include "TimerV2.hpp"
---
+++ project/TimerV2.hpp
@@ -0,0 +1,15 @@
+//
+// Created by asar on 10.07.16.
+//
+
+#ifndef EGNYTE_TIMERV2_HPP
+#define EGNYTE_TIMERV2_HPP
+
+
+class TimerV2
+{
+
+};
+
+
+#endif //EGNYTE_TIMERV2_HPP
